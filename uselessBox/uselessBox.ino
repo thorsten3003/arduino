716b81c5f513d6b39a69be4b1a8ee91ddd283621
Thorsten Stratmann <thorsten@tstratmann.de>
2016-12-17 21:50:28 +0100

    Toene in useless Box

--- uselessBox/uselessBox.ino
+++ uselessBox/uselessBox.ino
@@ -1,5 +1,6 @@
 
 #include "Servo.h"
+#include "musical_notes.h"
 unsigned long LEDprev; //Zeitspeicher für LED
 unsigned long USprev; //Zeitspeicher für Ultraschall
 unsigned long ServoArmprev; //Zeitspeicher für Servo
@@ -13,6 +14,7 @@ int LEDIntervall = 700; //1 Sekunde
 int LEDStatus = HIGH;
 int SchalterPin = 8;
 int SchalterStatus;
+int speakerPin = 6; // speaker connected to digital pin 6
 #define trigger 9  // Arduino Pin an HC-SR04 Trig
 #define echo 10     // Arduino Pin an HC-SR04 Echo
 long duration = 0;
@@ -36,6 +38,7 @@ void setup() {
   pinMode(trigger, OUTPUT);
   pinMode(echo, INPUT);
 
+  pinMode(speakerPin, OUTPUT); // sets the speakerPin to be an output
   // Nach dem Strom anstecken alles auf 0
   digitalWrite(LEDgruen, LEDStatus);
   ServoArm.write(0);
@@ -77,6 +80,7 @@ void loop() {
             break;
           }
         }
+        ohhh();
         zufall = 0;
         break;
       case 2: // schnell Deckel heben,schnell ausschalten
@@ -122,12 +126,12 @@ void loop() {
           StelleDeckel(53, 0); //Winkel , Wartezeit in ms
           BewegeArm(120, 15, true); // Stopposition, speed(delay) in ms  , Ultraschall
           if (z == 1)
-          {  Serial.println("z == 1");
+          { Serial.println("z == 1");
             z = 0; Grundstellung();
           }
           else
-          {  Serial.println("else Teil z == 0");
-            delay(500);
+          { Serial.println("else Teil z == 0");
+            ohhh();   //delay(500);
             SchalterAUS();
             BewegeArm(0, 5, false); //  Stopposition, speed(delay) in ms  , Ultraschall
             BewegeDeckel(0, 15, false); // Stopposition, speed(delay) in ms
@@ -147,6 +151,7 @@ void loop() {
           StelleDeckel(45, 1500);  //Winkel , Wartezeit in ms
           StelleDeckel(0, 3000);  //Winkel , Wartezeit in ms
           zufall = 3; // beim nächsten Durchlauf ausmachen
+          delay(2000);
         }
         break;
 
@@ -159,11 +164,11 @@ void loop() {
           StelleDeckel(53, 0);  //Winkel , Wartezeit in ms
           BewegeArm(180, 15, true); // Stopposition, speed(delay) in ms  , Ultraschall
           if (z == 1)
-          {  Serial.println("z == 1");
+          { Serial.println("z == 1");
             z = 0; Grundstellung();
           }
           else
-          {  Serial.println("else Teil z == 0");
+          { Serial.println("else Teil z == 0");
             SchalterAUS();
             BewegeArm(0, 15, false); // Stopposition, speed(delay) in ms  , Ultraschall
             BewegeDeckel(0, 15, false); // Stopposition, speed(delay) in ms
@@ -180,11 +185,11 @@ void loop() {
           StelleDeckel(53, 0);  //Winkel , Wartezeit in ms
           BewegeArm(165, 15, true); // Stopposition, speed(delay) in ms  , Ultraschall
           if (z == 1)
-          {  Serial.println("z == 1");
+          { Serial.println("z == 1");
             z = 0; Grundstellung();
           }
           else
-          {  Serial.println("else Teil z == 0");
+          { Serial.println("else Teil z == 0");
             delay(1200);
             SchalterAUS();
             BewegeArm(0, 3, false); // Stopposition, speed(delay) in ms  , Ultraschall
@@ -202,11 +207,11 @@ void loop() {
           StelleDeckel(53, 0);  //Winkel , Wartezeit in ms
           BewegeArm(180, 15, true); // Stopposition, speed(delay) in ms  , Ultraschall
           if (z == 1)
-          {  Serial.println("z == 1");
+          { Serial.println("z == 1");
             z = 0; Grundstellung();
           }
           else
-          {  Serial.println("else Teil z == 0");
+          { Serial.println("else Teil z == 0");
             SchalterAUS();
             BewegeArm(0, 15, false); //  Stopposition, speed(delay) in ms  , Ultraschall
           }
@@ -247,7 +252,8 @@ void loop() {
   { if (debug) Serial.println("else - Schalter aus");
     if ((millis() - ServoArmprev) > 1200000) //12000 Sekunden?
     {
-      AusZufall = random(1, 3);
+      AusZufall = random(1, 5);
+      int SoundZufall;
       switch (AusZufall)
       {
         case 1:  // Deckel klappern
@@ -265,8 +271,54 @@ void loop() {
           if (debug) Serial.println("case Aus2");
           BewegeDeckel(55, 25, true); // Stopposition, speed(delay) in ms
           StelleDeckel(53, 100);  //Winkel , Wartezeit in ms
+          squeak();
           while ( ! doUltraschall() ) {}
           Grundstellung();
+          break;
+
+        case 3:  // Sound
+          if (debug) Serial.println("case Aus3");
+          BewegeDeckel(15, 25, true); // Stopposition, speed(delay) in ms
+          StelleDeckel(13, 0);  //Winkel , Wartezeit in ms
+
+          SoundZufall = random(1, 3);
+          switch (SoundZufall)
+          {
+            case 1:
+              closeEncounters();
+              break;
+            case 2:
+              ariel();
+              break;
+          }
+          break;
+        case 4:  // Sound
+          if (debug) Serial.println("case Aus4");
+          SoundZufall = random(1, 8);
+          switch (SoundZufall)
+          {
+            case 1:
+              squeak();
+              break;
+            case 2:
+              catcall();
+              break;
+            case 3:
+              ohhh();
+              break;
+            case 4:
+              laugh();
+              break;
+            case 5:
+              laugh2();
+              break;
+            case 6:
+              waka();
+              break;
+            case 7:
+              r2D2();
+              break;
+          }
           break;
       }
 
@@ -380,7 +432,10 @@ void BewegeDeckel(int stoppos, int speedms, boolean Ultraschall)
       for (ServoDeckelpos; ServoDeckelpos < stoppos + 2; ServoDeckelpos++)
       {
         if (ServoDeckelpos >= Ultraschalldeckel && Ultraschall == true ) {
-          if(doUltraschall() ==1 ) {ServoDeckelpos=stoppos+2; break;}
+          if (doUltraschall() == 1 ) {
+            ServoDeckelpos = stoppos + 2;
+            break;
+          }
         }
         StelleDeckel(ServoDeckelpos, speedms); //Winkel , Wartezeit in ms
       }
@@ -391,7 +446,10 @@ void BewegeDeckel(int stoppos, int speedms, boolean Ultraschall)
       for (ServoDeckelpos; ServoDeckelpos > stoppos; ServoDeckelpos--)
       {
         if (ServoDeckelpos >= Ultraschalldeckel && Ultraschall == true ) {
-          if(doUltraschall() ==1 ) {ServoDeckelpos=stoppos; break;}
+          if (doUltraschall() == 1 ) {
+            ServoDeckelpos = stoppos;
+            break;
+          }
         }
         StelleDeckel(ServoDeckelpos, speedms); //Winkel , Wartezeit in ms
       }
@@ -409,7 +467,10 @@ void BewegeArm(int stoppos, int speedms, boolean Ultraschall)
       for (ServoArmpos; ServoArmpos < stoppos; ServoArmpos++)
       {
         if (ServoDeckelpos >= Ultraschalldeckel && Ultraschall == true ) {
-          if(doUltraschall() ==1 ) {ServoArmpos=stoppos; break;}
+          if (doUltraschall() == 1 ) {
+            ServoArmpos = stoppos;
+            break;
+          }
         }
         StelleArm(ServoArmpos, speedms); //Winkel , Wartezeit in ms
       }
@@ -420,12 +481,273 @@ void BewegeArm(int stoppos, int speedms, boolean Ultraschall)
       for (ServoArmpos; ServoArmpos > stoppos; ServoArmpos--)
       {
         if (ServoDeckelpos >= Ultraschalldeckel && Ultraschall == true ) {
-          if(doUltraschall() ==1 ) {ServoArmpos=stoppos; break;}
+          if (doUltraschall() == 1 ) {
+            ServoArmpos = stoppos;
+            break;
+          }
         }
         StelleArm(ServoArmpos, speedms); //Winkel , Wartezeit in ms
       }
       StelleArm(stoppos, 0); //Winkel , Wartezeit in ms
     }
   }
+}
+void beep (int speakerPin, float noteFrequency, long noteDuration)
+{
+  int x;
+  // Convert the frequency to microseconds
+  float microsecondsPerWave = 1000000 / noteFrequency;
+  // Calculate how many HIGH/LOW cycles there are per millisecond
+  float millisecondsPerCycle = 1000 / (microsecondsPerWave * 2);
+  // Multiply noteDuration * number or cycles per millisecond
+  float loopTime = noteDuration * millisecondsPerCycle;
+  // Play the note for the calculated loopTime.
+  for (x = 0; x < loopTime; x++)
+  {
+    digitalWrite(speakerPin, HIGH);
+    delayMicroseconds(microsecondsPerWave);
+    digitalWrite(speakerPin, LOW);
+    delayMicroseconds(microsecondsPerWave);
+  }
+}
 
+void scale()
+{
+  beep(speakerPin, note_C7, 500); //C: play the note C for 500ms
+  beep(speakerPin, note_D7, 500); //D
+  beep(speakerPin, note_E7, 500); //E
+  beep(speakerPin, note_F7, 500); //F
+  beep(speakerPin, note_G7, 500); //G
+  beep(speakerPin, note_A7, 500); //A
+  beep(speakerPin, note_B7, 500); //B
+  beep(speakerPin, note_C8, 500); //C
+}
+
+void r2D2() {
+  beep(speakerPin, note_A7, 100); //A
+  beep(speakerPin, note_G7, 100); //G
+  beep(speakerPin, note_E7, 100); //E
+  beep(speakerPin, note_C7, 100); //C
+  beep(speakerPin, note_D7, 100); //D
+  beep(speakerPin, note_B7, 100); //B
+  beep(speakerPin, note_F7, 100); //F
+  beep(speakerPin, note_C8, 100); //C
+  beep(speakerPin, note_A7, 100); //A
+  beep(speakerPin, note_G7, 100); //G
+  beep(speakerPin, note_E7, 100); //E
+  beep(speakerPin, note_C7, 100); //C
+  beep(speakerPin, note_D7, 100); //D
+  beep(speakerPin, note_B7, 100); //B
+  beep(speakerPin, note_F7, 100); //F
+  beep(speakerPin, note_C8, 100); //C
+}
+
+void closeEncounters() {
+  beep(speakerPin, note_Bb5, 300); //B b
+  delay(50);
+  beep(speakerPin, note_C6, 300); //C
+  delay(50);
+  beep(speakerPin, note_Ab5, 300); //A b
+  delay(50);
+  beep(speakerPin, note_Ab4, 300); //A b
+  delay(50);
+  beep(speakerPin, note_Eb5, 500); //E b
+  delay(500);
+
+  beep(speakerPin, note_Bb4, 300); //B b
+  delay(100);
+  beep(speakerPin, note_C5, 300); //C
+  delay(100);
+  beep(speakerPin, note_Ab4, 300); //A b
+  delay(100);
+  beep(speakerPin, note_Ab3, 300); //A b
+  delay(100);
+  beep(speakerPin, note_Eb4, 500); //E b
+  delay(500);
+
+  beep(speakerPin, note_Bb3, 300); //B b
+  delay(200);
+  beep(speakerPin, note_C4, 300); //C
+  delay(200);
+  beep(speakerPin, note_Ab3, 300); //A b
+  delay(500);
+  beep(speakerPin, note_Ab2, 300); //A b
+  delay(550);
+  beep(speakerPin, note_Eb3, 500); //E b
+}
+
+void ariel() {
+
+  beep(speakerPin, note_C6, 300); //C
+  delay(50);
+  beep(speakerPin, note_D6, 300); //D
+  delay(50);
+  beep(speakerPin, note_Eb6, 600); //D#
+  delay(250);
+
+  beep(speakerPin, note_D6, 300); //D
+  delay(50);
+  beep(speakerPin, note_Eb6, 300); //D#
+  delay(50);
+  beep(speakerPin, note_F6, 600); //F
+  delay(250);
+
+  beep(speakerPin, note_C6, 300); //C
+  delay(50);
+  beep(speakerPin, note_D6, 300); //D
+  delay(50);
+  beep(speakerPin, note_Eb6, 500); //D#
+  delay(50);
+  beep(speakerPin, note_D6, 300); //D
+  delay(50);
+  beep(speakerPin, note_Eb6, 300); //D#
+  delay(50);
+  beep(speakerPin, note_D6, 300); //D
+  delay(50);
+  beep(speakerPin, note_Eb6, 300); //D#
+  delay(50);
+  beep(speakerPin, note_F6, 600); //F
+  delay(50);
+
+}
+
+
+void laugh2() {
+  beep(speakerPin, note_C6, 200); //C
+  beep(speakerPin, note_E6, 200); //E
+  beep(speakerPin, note_G6, 200); //G
+  beep(speakerPin, note_C7, 200); //C
+  beep(speakerPin, note_C6, 200); //C
+  delay(50);
+  beep(speakerPin, note_C6, 200); //C
+  beep(speakerPin, note_E6, 200); //E
+  beep(speakerPin, note_G6, 200); //G
+  beep(speakerPin, note_C7, 200); //C
+  beep(speakerPin, note_C6, 200); //C
+  delay(50);
+  beep(speakerPin, note_C6, 50); //C
+  delay(50);
+  beep(speakerPin, note_C6, 50); //C
+  delay(50);
+  beep(speakerPin, note_C6, 50); //C
+  delay(50);
+  beep(speakerPin, note_C6, 50); //C
+  delay(50);
+  beep(speakerPin, note_C6, 50); //C
+  delay(50);
+  beep(speakerPin, note_C6, 50); //C
+  delay(50);
+  beep(speakerPin, note_C6, 50); //C
+
+
+
+}
+
+void squeak() {
+  for (int i = 100; i < 5000; i = i * 1.45) {
+    beep(speakerPin, i, 60);
+  }
+  delay(10);
+  for (int i = 100; i < 6000; i = i * 1.5) {
+    beep(speakerPin, i, 20);
+  }
+}
+
+void waka() {
+  for (int i = 1000; i < 3000; i = i * 1.05) {
+    beep(speakerPin, i, 10);
+  }
+  delay(100);
+  for (int i = 2000; i > 1000; i = i * .95) {
+    beep(speakerPin, i, 10);
+  }
+  for (int i = 1000; i < 3000; i = i * 1.05) {
+    beep(speakerPin, i, 10);
+  }
+  delay(100);
+  for (int i = 2000; i > 1000; i = i * .95) {
+    beep(speakerPin, i, 10);
+  }
+  for (int i = 1000; i < 3000; i = i * 1.05) {
+    beep(speakerPin, i, 10);
+  }
+  delay(100);
+  for (int i = 2000; i > 1000; i = i * .95) {
+    beep(speakerPin, i, 10);
+  }
+  for (int i = 1000; i < 3000; i = i * 1.05) {
+    beep(speakerPin, i, 10);
+  }
+  delay(100);
+  for (int i = 2000; i > 1000; i = i * .95) {
+    beep(speakerPin, i, 10);
+  }
+}
+
+void catcall() {
+  for (int i = 1000; i < 5000; i = i * 1.05) {
+    beep(speakerPin, i, 10);
+  }
+  delay(300);
+
+  for (int i = 1000; i < 3000; i = i * 1.03) {
+    beep(speakerPin, i, 10);
+  }
+  for (int i = 3000; i > 1000; i = i * .97) {
+    beep(speakerPin, i, 10);
+  }
+}
+
+void ohhh() {
+  for (int i = 1000; i < 2000; i = i * 1.02) {
+    beep(speakerPin, i, 10);
+  }
+  for (int i = 2000; i > 1000; i = i * .98) {
+    beep(speakerPin, i, 10);
+  }
+}
+
+void uhoh() {
+  for (int i = 1000; i < 1244; i = i * 1.01) {
+    beep(speakerPin, i, 30);
+  }
+  delay(200);
+  for (int i = 1244; i > 1108; i = i * .99) {
+    beep(speakerPin, i, 30);
+  }
+}
+
+void laugh() {
+  for (int i = 1000; i < 2000; i = i * 1.10) {
+    beep(speakerPin, i, 10);
+  }
+  delay(50);
+  for (int i = 1000; i > 500; i = i * .90) {
+    beep(speakerPin, i, 10);
+  }
+  delay(50);
+  for (int i = 1000; i < 2000; i = i * 1.10) {
+    beep(speakerPin, i, 10);
+  }
+  delay(50);
+  for (int i = 1000; i > 500; i = i * .90) {
+    beep(speakerPin, i, 10);
+  }
+  delay(50);
+  for (int i = 1000; i < 2000; i = i * 1.10) {
+    beep(speakerPin, i, 10);
+  }
+  delay(50);
+  for (int i = 1000; i > 500; i = i * .90) {
+    beep(speakerPin, i, 10);
+  }
+  delay(50);
+  for (int i = 1000; i < 2000; i = i * 1.10) {
+    beep(speakerPin, i, 10);
+  }
+  delay(50);
+  for (int i = 1000; i > 500; i = i * .90) {
+    beep(speakerPin, i, 10);
+  }
+  delay(50);
 }
